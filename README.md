0d4749b006bdf8ad77e7be39157f3ed3e409f2a9
Adam Gooch <agooch@8thlight.com>
2012-12-07 14:38:46 -0500

    Project has just been created

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a little application used to explore the common SQL injection attack.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSqlInjection::Application.config.secret_token = '84411727f801b6aa5d354c0e5f029762e559772a3d4bd7e831952378858522a6d8745792ae352be7f8434cb2586b47b5ac3dee4bc5e58e084ce6420ad75313d0'
